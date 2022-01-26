93e13cd9f0ef1199b21424b31c9b03cd48a43657
cos99 <andreicostea1.618@gmail.com>
2022-01-26 09:52:37 +0200

    Error handling Small upgrade of the nav bar

---
+++ client/src/app/errors/server-error/server-error.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-server-error',
+  templateUrl: './server-error.component.html',
+  styleUrls: ['./server-error.component.css']
+})
+export class ServerErrorComponent implements OnInit {
+  error: any;
+  messsage: string;
+
+  constructor(private router: Router) {
+    const navigation = this.router.getCurrentNavigation();
+    this.error = navigation?.extras?.state?.['error'];
+    this.messsage = navigation?.extras?.state?.['error'].message;
+   }
+
+  ngOnInit(): void {
+  }
+
+}
