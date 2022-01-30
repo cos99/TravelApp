46ad49879ec4e418e5ef6e67a215fdf7885585cb
cos99 <andreicostea1.618@gmail.com>
2022-01-31 00:33:50 +0200

    Add update of user add persistence add interceptor for loading add guard for preventing unsaved changes lost Small other updates

---
+++ API/DTOs/MemberUpdateDTO.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.DTOs
+{
+    public class MemberUpdateDTO
+    {
+        public string LookingFor { get; set; }
+        public string Interests { get; set; }
+        public string Country { get; set; }
+    }
+}
