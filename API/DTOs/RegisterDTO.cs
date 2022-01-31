f97faf6ed8a17d6e2739de97b6eff0d5510d7a11
cos99 <andreicostea1.618@gmail.com>
2022-01-31 14:25:00 +0200

    Update register

--- API/DTOs/RegisterDTO.cs
+++ API/DTOs/RegisterDTO.cs
@@ -12,6 +12,18 @@ namespace API.DTOs
         public string Username { get; set; }
 
         [Required]
+        public string Alias { get; set; }
+
+        [Required]
+        public string Gender { get; set; }
+
+        [Required]
+        public string Country { get; set; }
+
+        [Required]
+        public DateTime DateOfBirth { get; set; }
+
+        [Required]
         [StringLength(8, MinimumLength = 4)]
         public string Password { get; set; }
     }
